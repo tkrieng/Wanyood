796c355b606a2ac1ca3877604335466865280b32
top <t.kriengkomol@gmail.com>
2019-10-23 23:52:33 +0800

    Add style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { isHoliday } from './holidays';
+import './App.css';
 
 const today = new Date()
+var tomorrow = new Date()
 const wanyood = isHoliday(today) ? 'เออ หยุด' : 'หยุดพ่อมึง'
 
 function App() {
   return (
     <div className="App">
-      <p>
+      <p className="question">
+        วันนี้หยุดปะ?
+      </p>
+      <p className="answer">
         {wanyood}
       </p>
     </div >
