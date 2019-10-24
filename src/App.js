ad98a5e1ede3b0aea3a44a1f78bdcb1dec021765
top <t.kriengkomol@gmail.com>
2019-10-24 23:04:25 +0800

    Added sound

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { isHoliday } from './holidays';
 import './App.css';
 
-const today = new Date()
+var today = new Date()
 var tomorrow = new Date(today.toDateString())
 tomorrow.setDate(tomorrow.getDate() + 1)
-const wanyood = isHoliday(tomorrow) ? 'เออ หยุด' : 'หยุดพ่อมึง'
+const wanyood = isHoliday(tomorrow)
+var wanyoodText = wanyood ? 'เออ หยุด' : 'หยุดพ่อมึง'
+var soundFile = wanyood ? './yay.wav' : './fuck.wav'
+var sound = new Audio(soundFile)
+sound.load()
+sound.play()
 
 function App() {
   return (
@@ -14,7 +19,7 @@ function App() {
         พรุ่งนี้หยุดปะ?
       </p>
       <p className="answer">
-        {wanyood}
+        {wanyoodText}
       </p>
     </div >
   );
--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+beforeEach(() => {
+  HTMLMediaElement.prototype.load = jest.fn()
+  HTMLMediaElement.prototype.play = jest.fn()
+})
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
